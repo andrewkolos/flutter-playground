78a6c103cb8c84a14a49416ae70b209d1a147f57
Andrew Kolos <andrewrkolos@gmail.com>
2022-06-20 21:28:42 -0400

    feat: initial commit

---
+++ building_layouts_official_tutorial/android/app/src/main/kotlin/com/example/building_layouts_official_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.building_layouts_official_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ building_layouts_official_tutorial/lib/main.dart
@@ -0,0 +1,93 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Layout Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [],
+          )
+        ]);
+  }
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class TitleSection extends StatelessWidget {
+  final String title;
+
+  final String location;
+
+  final int starCount;
+
+  const TitleSection(
+      {Key? key,
+      required this.title,
+      required this.location,
+      required this.starCount})
+      : super(key: key);
+}
+
+class IconButton extends StatelessWidget {
+  const IconButton({Key? key, required this.text, required this.icon})
+      : super(key: key);
+
+  final Icon icon;
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [icon, Text(text)],
+    );
+  }
+}
